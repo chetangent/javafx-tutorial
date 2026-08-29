927fe0b32326a110e0d32e08afa623947f036524
Chetan <chetangips@gmail.com>
2026-08-29 22:02:40 +0800

    Tutorial-Part5

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,6 @@
 public class Duke {
+    private String commandType;
+
     public static void main(String[] args) {
         System.out.println("Hello!");
     }
@@ -7,6 +9,11 @@ public class Duke {
      * Generates a response for the user's chat message.
      */
     public String getResponse(String input) {
+        commandType = null;
         return "Duke heard: " + input;
+    }
+
+    public String getCommandType() {
+        return commandType;
     }
 }
